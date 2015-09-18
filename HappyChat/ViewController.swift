891882e43c7ab71629b1b1671c33d373058c7417
Dorothy Thurston <dorothy.thur@gmail.com>
2015-09-17 21:50:57 -0400

    Initial Commit

---
+++ HappyChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HappyChat
+//
+//  Created by Dorothy Thurston on 9/17/15.
+//  Copyright © 2015 Dorothy Thurston. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
