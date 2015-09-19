3ec1b584153e57ba20427d27c5abe8ef5eba49e8
Dorothy Thurston <dorothy.thur@gmail.com>
2015-09-19 00:14:14 -0400

    Implement basic chat like user experience with table view

---
+++ HappyChat/Message.swift
@@ -0,0 +1,24 @@
+//
+//  Message.swift
+//  HappyChat
+//
+//  Created by Dorothy Thurston on 9/18/15.
+//  Copyright © 2015 Dorothy Thurston. All rights reserved.
+//
+
+import Foundation
+
+/*
+struct Message {
+    var beenRead: Bool?
+    var beenSent: Bool?
+    var createdDate: NSDate?
+    var text: String?
+    
+    init(text: String) {
+        self.beenSent = false
+        self.beenRead = false
+        self.text = text
+    }
+}
+*/
--- HappyChat/MessagesViewController.swift
+++ HappyChat/MessagesViewController.swift
@@ -8,12 +8,21 @@
 
 import UIKit
 
-class MessagesViewController: UIViewController, UINavigationControllerDelegate  {
+class MessagesViewController: UIViewController, UINavigationControllerDelegate, UITableViewDataSource, UITableViewDelegate  {
+    //MARK: - Variables that will be deleted later
+    var exampleMessages = ["Hello!", "I am happy you exist"]
+    var storedChoice = "sent a message"
+    
+    //MARK: - Properties
 
     @IBOutlet weak var myMessage: UIButton!
+    @IBOutlet weak var messageTableView: UITableView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        messageTableView.delegate = self
+        messageTableView.dataSource = self
 
         // Do any additional setup after loading the view.
     }
@@ -23,7 +32,13 @@ class MessagesViewController: UIViewController, UINavigationControllerDelegate  
         // Dispose of any resources that can be recreated.
     }
     
+    //MARK: - Interface Actions
     @IBAction func didPressNewMessage(sender: AnyObject) {
+    }
+    
+    @IBAction func didPressSendMessage(sender: UIBarButtonItem) {
+        exampleMessages.append(storedChoice)
+        messageTableView.reloadData()
     }
     
     // MARK: - Navigation
@@ -35,12 +50,39 @@ class MessagesViewController: UIViewController, UINavigationControllerDelegate  
         }
     }
     
+    //MARK: - Table View Functions and related properties
+    let textCellIdentifier = "messageCell"
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+ 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return exampleMessages.count
+    }
 
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(textCellIdentifier, forIndexPath: indexPath) as UITableViewCell
+        
+        let row = indexPath.row
+        cell.textLabel?.text = exampleMessages[row]
+        
+        return cell
+    }
+    
+    // MARK:  UITableViewDelegate Methods
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+        let row = indexPath.row
+        print(exampleMessages[row])
+    }
+    
 }
 
 extension MessagesViewController: UIPickerViewControllerDelegate {
     func newMessageChoiceMade(choice: String) {
       myMessage.setTitle(choice, forState: UIControlState.Normal)
+        storedChoice = choice
    }
 }
 
